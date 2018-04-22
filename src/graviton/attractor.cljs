c6817f1c12d8080924a4bde0406772b2d82b8250
dmitri.sotnikov@gmail.com <dmitri.sotnikov@gmail.com>
2018-04-22 16:58:18 -0400

    switched attractor from sprite to graphics

--- src/graviton/attractor.cljs
+++ src/graviton/attractor.cljs
@@ -2,11 +2,20 @@
   (:require
     [graviton.engine :as engine]))
 
+(defn circle [color x y radius]
+  (doto (js/PIXI.Graphics.)
+    (.lineStyle 0)
+    (.beginFill color 1)
+    (.drawCircle 0 0 radius)
+    (.endFill)))
+
 (defn instance [idx x y mass]
-  {:id     (keyword (str "attractor-" (inc idx)))
-   :x      x
-   :y      y
-   :mass   mass
-   :width  (js/Math.ceil (* 50 (engine/sigmoid (/ (- mass 20) 100))))
-   :height (js/Math.ceil (* 50 (engine/sigmoid (/ (- mass 20) 100))))
-   :sprite (engine/sprite "circle.png")})
+  (let [width  (js/Math.ceil (* 50 (engine/sigmoid (/ (- mass 20) 100))))
+        height (js/Math.ceil (* 50 (engine/sigmoid (/ (- mass 20) 100))))]
+    {:id     (keyword (str "attractor-" (inc idx)))
+     :x      x
+     :y      y
+     :mass   mass
+     :width  width
+     :height height
+     :sprite (circle 0x0000FF x y (/ width 2))}))
